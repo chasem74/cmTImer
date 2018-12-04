7ace0d4054de8e2a47b21cf1a8add61a7a08fa15
Chase Metzger <chasemetzger15@gmail.com>
2018-12-03 18:44:59 -0800

    Test code for typescript and useHooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button
+} from 'react-native';
+
+interface CounterProps{
+	initialValue: number
+}
+
+function Counter({initialValue}: CounterProps) {
+	let [count, setCount] = useState(initialValue);
+
+	return <Button onPress={() => setCount(count + 1)} title={count.toString()} />
+}
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </View>
-    );
-  }
+	public render() {
+		return (
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your app!</Text>
+				<Counter initialValue={4}/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
